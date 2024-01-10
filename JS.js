4ed570abd1660cf4b88e0663811ed918ba530ab2
csizmarikb <boldizsarcsizmarik@gmail.com>
2024-01-10 12:25:19 +0100

    css,js,index,2

--- JS.js
+++ JS.js
@@ -1,9 +1,22 @@
 var box = document.getElementById("box")
+var b1 = document.getElementById("b1")
+var b2 = document.getElementById("b2")
+b2.disabled = true
+w = window.getComputedStyle(box.children[0]).getPropertyValue("width")
 
-document.getElementById("b1").onclick = function(){
+b1.onclick = function(){
     console.log("click")
-    box.style.marginLeft = "200px"
+    
+    console.log(w)
+    box.style.transform = "translate(-"+w+",0px)"
+    this.disabled = true
+    b2.disabled = false
 }
 
-
+b2.onclick = function(){
+    console.log("click")
+    box.style.transform = "translate(0px,0px)"
+    this.disabled = true
+    b1.disabled = false
+}
 
