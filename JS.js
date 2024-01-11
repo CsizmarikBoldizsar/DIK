d9189a11f88647d7e4790e5e348a8d4bba4342f0
CsBoldi <csizibozsi@gmail.com>
2024-01-11 21:19:52 +0100

    map

--- JS.js
+++ JS.js
@@ -1,22 +1,35 @@
 var box = document.getElementById("box")
 var b1 = document.getElementById("b1")
 var b2 = document.getElementById("b2")
+var pins = document.getElementsByClassName("pinButton")
+
+
 b2.disabled = true
 w = window.getComputedStyle(box.children[0]).getPropertyValue("width")
 
 b1.onclick = function(){
-    console.log("click")
-    
-    console.log(w)
     box.style.transform = "translate(-"+w+",0px)"
     this.disabled = true
     b2.disabled = false
 }
 
 b2.onclick = function(){
-    console.log("click")
     box.style.transform = "translate(0px,0px)"
     this.disabled = true
     b1.disabled = false
+}
+
+
+
+
+for (let i=0; i < pins.length; i++) {
+    pins[i].addEventListener("click",function(){
+        if (pins[i].children[1].style.display == "block"){
+            pins[i].children[1].style.display = "none"
+        }
+        else{
+            pins[i].children[1].style.display = "block"
+        }
+    })
 }
 
