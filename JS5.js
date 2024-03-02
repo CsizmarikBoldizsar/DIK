e874647d7ecfd623af0bbd32f1e9b0ef1a5ee0ed
CsBoldi <csizibozsi@gmail.com>
2024-03-02 22:32:17 +0100

    jrtjrtjjr

--- JS5.js
+++ JS5.js
@@ -1,26 +1,30 @@
 
 window.onload = function(){
   var resultsChildren = document.getElementById("results").children    
-  for(let i = 0; i < resultsChildren.length; i++){
-    console.log("-------------------------------")
-    var c = resultsChildren.item(i)    
-    c.href = c.id+".html"
   
-    console.log("resultsChildren--- ",resultsChildren)
-    console.log("c--- ",c)
-
-    var path = "Details/"+c.id.toLowerCase() + ".txt"
-    var imgPath = "Images/"+c.id+"/"+c.id+" ("
-
-    console.log("path--- ",path)
-    console.log("imgPath--- ",imgPath)
-
-    fetch(path)
+  fetch(path)
     .then(response => response.text())
     .then((data) => {      
 
       console.log("data--- ",data)
     
+
+      for(let i = 0; i < resultsChildren.length; i++){
+        console.log("-------------------------------")
+        var c = resultsChildren.item(i)    
+        c.href = c.id+".html"
+      
+        console.log("resultsChildren--- ",resultsChildren)
+        console.log("c--- ",c)
+    
+        var path = "Details/"+c.id.toLowerCase() + ".txt"
+        var imgPath = "Images/"+c.id+"/"+c.id+" ("
+    
+        console.log("path--- ",path)
+        console.log("imgPath--- ",imgPath)
+
+
+
       var splitData = data.split("\n")
       c.children[0].src = imgPath+splitData[splitData.indexOf("-img")+1]+").png"      
       c.children[1].children[0].innerHTML = splitData[1]+" M Ft"
@@ -30,9 +34,9 @@ window.onload = function(){
       console.log("t--- ",t)              
       t.children[0].children[0].innerHTML = splitData[2]+"m<sup>2</sup>"
       t.children[1].children[0].innerHTML = splitData[3]
-    })
+    
     console.log("-------------------------------")
-  }
-  
+    }
+  })
 }
 
