a504ec51359d2823dd02263705d8686cf6f42a22
CsBoldi <csizibozsi@gmail.com>
2024-03-02 22:35:43 +0100

    rereherh

--- JS5.js
+++ JS5.js
@@ -1,9 +1,13 @@
 
 window.onload = function(){
-  var resultsChildren = document.getElementById("results").children    
+  var resultsChildren = document.getElementById("results").children
+  
+  
   for(let i = 0; i < resultsChildren.length; i++){
+
     console.log("-------------------------------")
-    var c = resultsChildren.item(i)    
+
+    var c = resultsChildren.item(i)
     c.href = c.id+".html"
   
     console.log("resultsChildren--- ",resultsChildren)
@@ -22,16 +26,23 @@ window.onload = function(){
       console.log("data--- ",data)
     
       var splitData = data.split("\n")
-      c.children[0].src = imgPath+splitData[splitData.indexOf("-img")+1]+").png"      
+
+      console.log("src--- ",imgPath+splitData[splitData.indexOf("-img")+1]+").png")
+
+      c.children[0].src = imgPath+splitData[splitData.indexOf("-img")+1]+").png"
       c.children[1].children[0].innerHTML = splitData[1]+" M Ft"
       c.children[1].children[2].children[0].innerHTML = splitData[-1]
       c.children[1].children[2].children[1].innerHTML = splitData[0]
       var t = c.children[1].children[3].children[0].children[1]
-      console.log("t--- ",t)              
+
+      console.log("t--- ",t)
+
       t.children[0].children[0].innerHTML = splitData[2]+"m<sup>2</sup>"
       t.children[1].children[0].innerHTML = splitData[3]
     })
+
     console.log("-------------------------------")
+
   }
   
 }
