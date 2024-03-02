6a6785193b7ecbb32a158df48a5f4e2ea6be6725
CsBoldi <csizibozsi@gmail.com>
2024-03-02 22:18:06 +0100

    rehherehe

--- JS5.js
+++ JS5.js
@@ -1,32 +1,24 @@
 
 window.onload = function(){
-  var resultsChildren = document.getElementById("results").children
-  console.log(resultsChildren)
-  console.log("lloaded")
+  var resultsChildren = document.getElementById("results").children    
   for(let i = 1; i < resultsChildren.length; i++){
-    console.log(resultsChildren[i].id)
-    var c = resultsChildren.item(i)
-    console.log(c)
+    var c = resultsChildren.item(i)    
     c.href = c.id+".html"
   
+    console.log("c--- ",c)
+
     var path = "Details/"+c.id.toLowerCase() + ".txt"
-    var imgPath = "Images/"+c.id+"/"+c.id+" ("
-    console.log(c.id)
-    console.log(imgPath)
+    var imgPath = "Images/"+c.id+"/"+c.id+" ("        
     fetch(path)
     .then(response => response.text())
-    .then((data) => {
-      console.log(data)
+    .then((data) => {      
       var splitData = data.split("\n")
-      c.children[0].src = imgPath+splitData[splitData.indexOf("-img")+1]+").png"
-      console.log(imgPath+splitData[splitData.indexOf("-img")+1]+").png")
+      c.children[0].src = imgPath+splitData[splitData.indexOf("-img")+1]+").png"      
       c.children[1].children[0].innerHTML = splitData[1]+" M Ft"
       c.children[1].children[2].children[0].innerHTML = splitData[-1]
       c.children[1].children[2].children[1].innerHTML = splitData[0]
       var t = c.children[1].children[3].children[0].children[1]
-      console.log(c)
-      console.log(t)
-      console.log(t.children)
+      console.log("t--- ",t)              
       t.children[0].children[0].innerHTML = splitData[2]+"m<sup>2</sup>"
       t.children[1].children[0].innerHTML = splitData[3]
     })
