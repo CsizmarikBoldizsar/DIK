adde6ec8eac50654603755cb0dd3a1094a5e65f4
CsBoldi <csizibozsi@gmail.com>
2024-03-03 12:37:37 +0100

    rrgrrrgrgg

--- JS5.js
+++ JS5.js
@@ -1,7 +1,7 @@
 
 window.onload = function(){
   var resultsChildren = document.getElementById("results").children
-  console.log("resultsChildren--- ",resultsChildren)
+  //console.log("resultsChildren--- ",resultsChildren)
   
 
 
@@ -9,7 +9,7 @@ window.onload = function(){
 
 
 
-  var dataTable = []
+  //var dataTable = []
   var paths = []
   var imgPaths = []
 
@@ -17,13 +17,13 @@ window.onload = function(){
     var c = resultsChildren.item(i)
     var path = "Details/"+c.id.toLowerCase() + ".txt"
     var imgPath = "Images/"+c.id+"/"+c.id+" ("
-    console.log("pushing paths",i)
+    //console.log("pushing paths",i)
 
     paths.push(path)
     imgPaths.push(imgPath)
 
-    console.log(paths)
-    console.log(imgPaths)
+    //console.log(paths)
+    //console.log(imgPaths)
   }
 
 
@@ -32,22 +32,22 @@ window.onload = function(){
     fetch(paths[i])
     .then(response => response.text())
     .then((data) => {
-      console.log("pushing data",i)
+      //console.log("pushing data",i)
 
-      dataTable.push(data)
+      //dataTable.push(data)
 
-      console.log(dataTable)
+      //console.log(dataTable)
 
       //for(let i = 0; i < resultsChildren.length; i++){
         console.log("-------------------------------")
         var c = resultsChildren.item(i)
         c.href = c.id+".html"
-        console.log("c--- ",c)
+        //console.log("c--- ",c)
     
         //var splitData = dataTable[i].split("\n")
         var splitData = data.split("\n")
 
-        console.log("src--- ",imgPaths[i]+splitData[splitData.indexOf("-img")+1]+").png")
+        //console.log("src--- ",imgPaths[i]+splitData[splitData.indexOf("-img")+1]+").png")
     
         c.children[0].src = imgPaths[i]+splitData[splitData.indexOf("-img")+1]+").png"
         c.children[1].children[0].innerHTML = splitData[1]+" M Ft"
@@ -55,11 +55,11 @@ window.onload = function(){
         c.children[1].children[2].children[1].innerHTML = splitData[0]
         var t = c.children[1].children[3].children[0].children[1]
     
-        console.log("t--- ",t)
+        //console.log("t--- ",t)
     
         t.children[0].children[0].innerHTML = splitData[2]+"m<sup>2</sup>"
         t.children[1].children[0].innerHTML = splitData[3]
-        console.log("-------------------------------")
+        //console.log("-------------------------------")
       //}
     })
   }  
